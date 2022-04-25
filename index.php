8ae2f0c1e8693884adf3b5f5c92559b9ba42b26a
jamalraouj <jamalrwj@gmail.com>
2022-04-25 10:23:39 +0000

    I add some folder

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./View/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="./View/bootstrap/js/bootstrap.js">
+    <link rel="stylesheet" href="./View/bootstrap/font-awesome/css/font-awesome.css">
+    <title>gestion d’école</title>
+</head>
+<body>
+    
+</body>
+</html>
